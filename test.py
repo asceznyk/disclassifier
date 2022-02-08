d5cb4d923c47fedd11136b3ee39a6ac5b514eeef
asceznyk <asceznyk@gmail.com>
2022-02-08 14:20:15 +0530

    ..

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from dataset import *
+
 xcol, ycol = 'ptitle', 'Category'
 train_df, valid_df = preprocess_split_data(pd.read_excel('/kaggle/input/catpreds/train_set_corvid.xlsx'))
 train_loader = create_loader(train_df, xcol, ycol)
