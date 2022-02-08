7edb13f3356ae6aba6951a59ac72e5183f78261e
asceznyk <asceznyk@gmail.com>
2022-02-08 15:41:37 +0530

    ..

--- model.py
+++ model.py
@@ -19,5 +19,25 @@ class BertClassifier(nn.Module):
         
         return self.log_softmax(x)
 
+class BiGRUClassifier(nn.Module):
+    def __init__(self, n_class, vocab_size, hidden_dim, dropout=0.1):
+        super(BiGRUClassifier, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, 768)
+        self.gru = nn.GRU(input_size=768, 
+                          hidden_size=hidden_dim, 
+                          bidirectional=True)
+        self.dropout = nn.Dropout(dropout)  
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(hidden_dim*2, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, n_class)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, seq):
+        emb = self.embedding(seq)
+        out, _ = self.gru(emb.transpose(0,1)) ## time, batch, dim
+        x = self.fc1(out[-1, :, :]) ## taking the last time-step as input
+        x = self.relu(x)
+        x = self.fc2(x)
+        return self.log_softmax(x)
 
 
