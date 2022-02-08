92fcc42ef0475ab5e170a91fee7219e29e3fb977
asceznyk <asceznyk@gmail.com>
2022-02-08 17:23:55 +0530

    ..

--- test.py
+++ test.py
@@ -12,9 +12,6 @@ xcol, ycol = 'ptitle', 'Category'
 train_loader, valid_loader, n_class = split_preprocess_data(pd.read_csv('/kaggle/input/catpreds/train_set.csv'), xcol, ycol)
  
 master = BertClassifier(AutoModel.from_pretrained('bert-base-uncased'), n_class, HIDDEN_DIM)
-
-print(master)
-
 train(master, train_loader, valid_loader)
 calc_acc(master, valid_loader)
 
