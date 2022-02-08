c88529743831608187e3d60113333cb91b292662
asceznyk <asceznyk@gmail.com>
2022-02-08 19:29:57 +0530

    ..

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 
-cost = nn.NLLLoss() 
-
 class BertClassifier(nn.Module):
     def __init__(self, bert, n_class, hidden_dim, dropout=0.1):
         super(BertClassifier, self).__init__()
@@ -31,15 +29,13 @@ class BiGRUClassifier(nn.Module):
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(hidden_dim*2, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, n_class)
-        self.log_softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, seq):
         emb = self.embedding(seq)
         out, _ = self.gru(emb.transpose(0,1)) ## time, batch, dim
         x = self.fc1(out[-1,:,:]) ## taking the last time-step as input
         x = self.relu(x)
-        x = self.fc2(x)
 
-        return self.log_softmax(x)
+        return self.fc2(x)
 
 
