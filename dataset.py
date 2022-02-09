3946594314502ccaea532058beda3e2305ac7ef2
asceznyk <asceznyk@gmail.com>
2022-02-09 17:18:00 +0530

    ..

--- dataset.py
+++ dataset.py
@@ -41,7 +41,7 @@ def split_preprocess_data(df, xcol, ycol, nan_txt='Other', split=0.1):
     
     return create_loader(train_df), create_loader(valid_df, is_train=0), train_df, valid_df, y_uniq
 
-def predict(model, ckpt_path, test_df, labels, text_col, pred_col, label_col=None, n_samples=20):   
+def predict(model, ckpt_path, test_df, labels, text_col, pred_col, label_col=None, n_samples=50):   
     model.load_state_dict(torch.load(ckpt_path))
     model = model.cpu()
 
