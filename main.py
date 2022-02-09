7947ad03c563497a5e5a85e0383054594cd647ab
asceznyk <asceznyk@gmail.com>
2022-02-09 16:59:44 +0530

    ..

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from trainer import *
 from transformers import AutoModel
 
 def main(args):
-    N_CLASS = args.n_class
-
     is_train, xcol, ycol = args.is_train, args.input_col, args.label_col  
     student = BiGRUClassifier(N_CLASS, VOCAB_SIZE, EMB_DIM, HIDDEN_DIM) 
     student_path = 'best.student.classifier'
@@ -34,8 +32,16 @@ def main(args):
 
         pred_df = predict(student, student_path, valid_df, labels, xcol, pcol, ycol)
         pred_df.to_csv('valid_preds.csv', index=False)
-    else:
+
+        with open('labels.txt', 'w') as f:
+            for l in labels:
+                f.write('%s\n' % l)
+    else: 
         test_df = pd.read_csv(args.full_csv)
+        labels = []
+        with open('labels.txt', 'r') as f:
+            for l in f: labels.append(l.replace('\n', ''))
+
         student.load_state_dict(torch.load(student_path))
         pred_df = predict(student, student_path, test_df, labels, xcol, pcol)
         pred_df.to_csv('test_preds.csv', index=False)
@@ -43,7 +49,6 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--is_train', type=int, default=1, help='train mode and test')
-    parser.add_argument('--n_class', type=int, help='no of classes')
     parser.add_argument('--full_csv', type=str, help='train csv file with labels')
     parser.add_argument('--input_col', type=str, help='inputs to the seq classifier')
     parser.add_argument('--label_col', type=str, help='labels to the seq classifier')
