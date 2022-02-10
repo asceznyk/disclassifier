4aa48cfcd662f2fab38b5e64b83302b4729ed6a5
asceznyk <asceznyk@gmail.com>
2022-02-10 16:26:30 +0530

    ..

--- test.py
+++ test.py
@@ -30,7 +30,8 @@ def main():
 
     calc_acc(master, valid_loader)
     logits_df = predict_logits(master, train_df, xcol)
-    print(logits_df)
+    loader = create_loader(logits_df, xcol, 'logits')
+    print(next(iter(loader)))
 
     #fit(student, train_loader, valid_loader, student_path)
     #student.load_state_dict(torch.load(student_path))
